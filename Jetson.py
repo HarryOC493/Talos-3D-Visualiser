e337d66e2057bbb6c6694c3043ee774ffde38014
Harry O'Connor <harryoconnor007@gmail.com>
2023-12-16 20:38:51 +0000

    ---

--- Jetson.py
+++ Jetson.py
@@ -36,9 +36,6 @@ try:
             
             # Read data from the distance sensor
             distance_str = ser3.readline().decode('utf-8').strip()
-            distance = [str(dist) for dist in distance_str.split(',')]
-            print(distance[1])
-            message.extend(float(distance[1]))
 
 
             # Send the message to the MacBook
