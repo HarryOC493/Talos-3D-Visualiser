6fd2bf191a1fa766b8f4cf3a063b995c0b86bae1
Harry O'Connor <harryoconnor007@gmail.com>
2023-12-16 20:40:42 +0000

    ----

--- Jetson.py
+++ Jetson.py
@@ -3,8 +3,8 @@ import socket
 
 # Replace with the appropriate serial ports for your Arduino devices and their respective baud rates
 ser1 = serial.Serial('/dev/ttyACM0', 9600)
-ser2 = serial.Serial('/dev/ttyACM1', 9600)
-ser3 = serial.Serial('/dev/ttyACM2', 9600)
+ser3 = serial.Serial('/dev/ttyACM1', 9600)
+ser2 = serial.Serial('/dev/ttyACM2', 9600)
 
 # Replace with your MacBook's IP address and the chosen port number
 macbook_ip = '192.168.0.26'
