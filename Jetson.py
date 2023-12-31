a20c202e6572e9aeb6a4974fb39a7ed3f7f5690e
Harry O'Connor <harryoconnor007@gmail.com>
2023-12-31 23:38:56 +0000

    _

--- Jetson.py
+++ Jetson.py
@@ -2,8 +2,8 @@ import serial
 import socket
 
 # Replace with the appropriate serial ports for your Arduino devices and their respective baud rates
-ser1 = serial.Serial('/dev/ttyACM1', 9600)
-ser3 = serial.Serial('/dev/ttyACM0', 9600)
+ser1 = serial.Serial('/dev/ttyACM0', 9600)
+ser3 = serial.Serial('/dev/ttyACM1', 9600)
 ser2 = serial.Serial('/dev/ttyACM2', 9600)
 
 # Replace with your MacBook's IP address and the chosen port number
@@ -12,6 +12,7 @@ macbook_port = 50000
 
 # Create a socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 try:
     # Connect to the MacBook
     sock.connect((macbook_ip, macbook_port))
@@ -19,43 +20,24 @@ try:
 
     while True:
         try:
-            debug = 1
             message = []
-            left_contact, right_contact = None, None  # Initialize the contact variables
-
             # Read data from the joints
             positions = ser1.readline().decode('utf-8').strip()
             if positions:
-                if debug:
-                    print(positions)
-                data = positions.replace('[', '').replace(']', '').split(',')
-                # All elements except the last two are joint angles
-                joint_angles_degrees = [float(angle) for angle in data[:-2]]
-                # The second last element is the left contact binary
-                left_contact = int(data[-2])
-                # The last element is the right contact binary
-                right_contact = int(data[-1])
+                joint_angles_degrees = [float(angle) for angle in positions.replace('[', '').replace(']', '').split(',')]
                 message.extend(joint_angles_degrees)
-                # Add left and right contact at the end of the message
-                message.append(left_contact)
-                message.append(right_contact)
 
             # Read data from the imu
             angles = ser2.readline().decode('utf-8').strip()
             if angles:
-                if debug:
-                    print(angles)
-                data = angles.replace('[', '').replace(']', '').split(',')
-                imu_angles_degrees = [float(angle) for angle in data]
+                imu_angles_degrees = [float(angle) for angle in angles.replace('[', '').replace(']', '').split(',')]
                 message.extend(imu_angles_degrees)
 
+            
             # Read data from the distance sensor
-            distance_str = ser3.readline().decode('utf-8').strip()
-            if distance_str:
-                if debug:
-                    print(distance_str)
-                distance = float(distance_str)
-                message.append(distance)
+            distance_str = ser3.readline().decode('utf-8')
+            distance = float(distance_str)
+            message.append(distance)
 
             # Send the message to the MacBook
             serialized_message = ",".join(map(str, message))
@@ -69,14 +51,9 @@ try:
 
         except KeyboardInterrupt:
             # Close the socket when the script is interrupted
-            print("Interrupt received, stopping...")
+            sock.close()
             break
-
-        except Exception as e:
-            # Handle other exceptions
-            print(f"An unexpected error occurred: {e}")
 
 finally:
     # Ensure the socket is closed even if an exception occurs
-    print("Closing socket...")
     sock.close()
